b37324b26fab6172aacc988038ff9a2596f62f5a
chaudhuree <chaudhuree00@gmail.com>
2022-12-28 21:06:04 +0600

    json best practices

---
+++ _note.js
@@ -0,0 +1,39 @@
+// json best practices
+// a. use camelCase for property names : "firstName": "John
+// b. use double quotes for string & number values : "rollNo": "1"
+// c. use double quotes for key and value <both></both>
+// d. use special characters to replace different spaces like,
+//   - underscore for space, \b for backspace, \t for tab, \n for new line, \f for form feed, \r for carriage return, \v for vertical tab
+// e. use root level array for multiple objects
+
+// example: //ref: here menu and subMenu are root level arrays
+// {
+//   "menu": [
+//     {
+//       "id": "1",
+//       "name": "File",
+//       "value": "F"
+//     },
+//     {
+//       "id": "2",
+//       "name": "Edit",
+//       "value": "E"
+//     }
+
+//   ],
+//   "subMenu": [
+//     {
+//       "id": "3",
+//       "name": "Open",
+//       "value": "O"
+//     },
+//     {
+//       "id": "4",
+//       "name": "Close",
+//       "value": "C"
+//     }
+//   ]
+// }
+
+// JSON best practices ends
+
